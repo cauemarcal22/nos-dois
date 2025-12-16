45e9374a37a43e93687a30842b394d1dbf29351a
cauemarcal22 <caue.corporativo22@gmail.com>
2025-12-16 14:51:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function toggleVisibility(buttonId, messageId) {
   }
 }
 
-for (let i = 0; i <= 214; i++) {
+for (let i = 0; i <= 216; i++) {
   toggleVisibility(`clickHere${i}`, `message${i}`);
 }
 
@@ -118,7 +118,7 @@ if (img && audio) {
 }
 
 // ===== Contador desde uma data específica =====
-const dataReferencia = new Date("2025-12-14T23:32:00");
+const dataReferencia = new Date("2025-12-16T14:48:00");
 const timerEl = document.getElementById("timer");
 
 if (timerEl) {
